b5cf159cfe83ece792c9dd5e30c0fb4ee5d7f663
arun <arunyuvi0206@gmail.com>
2024-01-18 23:35:52 +0530

    bugs fixed

--- jwttoken.py
+++ jwttoken.py
@@ -20,6 +20,6 @@ def verify_token(token:str,credentials_exception):
         email: str = payload.get("sub")
         if email is None:
             raise credentials_exception
-        token_data = schemas.TokenData(email=email)
+        # token_data = schemas.TokenData(email=email)
     except JWTError:
         raise credentials_exception
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI,Response
 from database import engine,Base
 import models
-from routers import user,blog
+from routers import user,blog,authentication
 app = FastAPI()
 models.Base.metadata.create_all(engine)
 
@@ -11,3 +11,5 @@ models.Base.metadata.create_all(engine)
 
 
 app.include_router(user.router)
+app.include_router(blog.router)
+app.include_router(authentication.router)
