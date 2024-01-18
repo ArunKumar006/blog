9d3d9dc7616d25825481ec04f962c4dc49a50c8b
arun <arunyuvi0206@gmail.com>
2024-01-18 13:26:34 +0530

    update

--- repository/blog.py
+++ repository/blog.py
@@ -22,3 +22,22 @@ def dsestroy(id:int,db:Session):
     blog.delete(synchronize_session=False)
     db.commit()
     return "done"
+
+
+def update(id:int,request:schemas.Blog,db:Session):
+    blog=db.query(models.Blog).filter(models.Blog.id==id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with id {id} not found")
+    blog.update(request)
+    db.commit()
+    return "updated"
+
+
+def show(id:int,db:Session):
+    blog=db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                    detail=f"Blog with the id {id} is not available")
+    return blog
